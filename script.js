bd0041955557bbebcfaa7c8f68a194fa36ad32b0
Paularaya2 <67979443+paulybo@users.noreply.github.com>
2021-07-03 00:50:41 +0000

    Started Project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Author:
+// Author: Paul Araya
 
 // Global UI Variables
 //let canvasDiv;
@@ -7,7 +7,23 @@
 //let featureExtractor;
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model Loading, Please wait...");
+  textP.parent(textDiv);
+  textP2 = createP("[Training data here]");
+  textP2.parent(textDiv);
+  // build Buttons
+  buildButtons();
+  // initialize Ups, Downs, Lefts, Rights
+  ups = 0;
+  down = 0;
+  left = 0;
+  right = 0;
+  // load video
+  video = createCapture(VIDEO, videoReady);
 }
 
 function draw() {
